a2fdeffc109833222fab686fade7eeee8c6037f0
VaNyA1593 <150516015+VaNyA1593@users.noreply.github.com>
2026-08-09 11:23:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([-8.537, 37.136]),
+        zoom: 12
+    })
+});
